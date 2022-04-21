9c68cded022df50cf522f40db5ffe031cbac0491
ErolKocoglu <erolkocoglu44@gmail.com>
2022-04-21 18:05:14 +0300

    Sorting array elements and other fixes

--- average.c
+++ average.c
@@ -1,35 +1,35 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 
 #define SIZE 10
-#define WIN_SIZE 3
+#define WIN_SIZE 3*/
 
-int main() {
+/*int main() {
     double input[SIZE];
     for (int i = 0; i < SIZE; i++) {
         printf("Enter the value for %d \n", i + 1);
         scanf("%lf", &input[i]);
-    }
+    }*/
 
-    double cumulativeAvg[SIZE];
+    //double cumulativeAvg[SIZE];
 
-    double total = 0;
+    /*double total = 0;
     for (int i = 0; i < SIZE; i++) {
         total += input[i];
 
         cumulativeAvg[i] = total / (i + 1);
-    }
+    }*/
 
-    double movingAvg[SIZE] = {0};
+    //double movingAvg[SIZE] = {0};
 
-    for (int i = WIN_SIZE - 1; i < SIZE; i++) {
+    /*for (int i = WIN_SIZE - 1; i < SIZE; i++) {
         total = 0;
         for (int j = i; j > i - WIN_SIZE; j--) {
             total += input[j];
         }
         movingAvg[i] = total / WIN_SIZE;
-    }
+    }*/
     /*for (int i = 0; i <= SIZE - WIN_SIZE; i++) {
         total = 0;
         for (int j = i; j < j + WIN_SIZE; j++) {
@@ -38,19 +38,19 @@ int main() {
         movingAvg[i] = total / WIN_SIZE;
     }*/
 
-    printf("-Exchange Rate - Cumulative Average - Moving Average -\n");
-    for (int i = 0; i < SIZE; i++) {
+    //printf("-Exchange Rate - Cumulative Average - Moving Average -\n");
+    /*for (int i = 0; i < SIZE; i++) {
         printf("| %.3lf             | %.3lf             | %.3lf             |\n", input[i], cumulativeAvg[i],
                movingAvg[i]);
     }
     for (int i = 0; i < 55; i++) {
         printf("-");
     }
-    printf("\n");
+    printf("\n");*/
 
 
-    return 0;
-}
+    //return 0;
+//}
 
 //
 // Created by User on 14.04.2022.
---
+++ bubble_sort2.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int originArray[10];
+    printf("Enter 10 numbers:");
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &originArray[i]);
+    }
+    printf("Original Array:");
+    for (int i = 0; i < 10; i++) {
+        printf(" %d", originArray[i]);
+    }
+    printf("\n");
+
+    int length = 9;
+
+    for (int i = 0; i < length; i++) {
+        int tempValue;
+        bool isOk = false;
+        if (originArray[i] > originArray[i + 1]) {
+            tempValue = originArray[i + 1];
+            originArray[i + 1] = originArray[i];
+            originArray[i] = tempValue;
+        }
+        if (i == length - 1) {
+            for (int j = 0; j < length; j++) {
+                if (originArray[j] > originArray[j + 1]) {
+                    isOk = false;
+                    break;
+                }
+                if (j == length - 1) {
+                    isOk = true;
+                }
+            }
+            if (isOk == false) {
+                i = -1;
+                length--;
+            }
+        }
+
+
+    }
+
+    printf("Sorted Array(Ascending):");
+    for (int i = 0; i < 10; i++) {
+        printf(" %d", originArray[i]);
+    }
+    printf("\n");
+
+    printf("Sorted Array(Descending):");
+    for (int i = 9; i >= 0; i--) {
+        printf(" %d", originArray[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
+
+//
+// Created by User on 21.04.2022.
+//
+
